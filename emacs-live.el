a9951b4b0cd511df169c1814b9f91a8ad3c12c85
David Selassie <selassid@gmail.com>
2013-04-14 22:57:29 -0700

    Adds emacs-live.el with my custom pack.

---
+++ emacs-live.el
@@ -0,0 +1,6 @@
+(live-use-packs '(live/foundation-pack     ; Use live-use-packs to control which built-in packs should be loaded
+                  live/colour-pack         ; This built-in pack MUST be disabled if you want to use 3rd party color theme pack
+                  live/clojure-pack
+                  live/lang-pack
+                  live/power-pack))
+(live-add-packs '(~/.live-packs/selassid-pack))
