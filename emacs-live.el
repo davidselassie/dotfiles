719582a36ae5f5a6556ba71261331faa32187cdc
David Selassie <selassid@gmail.com>
2014-04-02 10:50:38 -0700

    Updates emacs-live to new directory structure.

--- emacs-live.el
+++ emacs-live.el
@@ -1,7 +1,9 @@
-(live-use-packs '(live/foundation-pack     ; Use live-use-packs to control which built-in packs should be loaded
-                  live/colour-pack         ; This built-in pack MUST be disabled if you want to use 3rd party color theme pack
-                  live/clojure-pack
-                  live/lang-pack
-                  live/power-pack))
-(live-add-packs '(live/bindings-pack))
+(live-use-packs '(stable/foundation-pack     ; Use live-use-packs to control which built-in packs should be loaded
+                  stable/colour-pack         ; This built-in pack MUST be disabled if you want to use 3rd party color theme pack
+                  stable/lang-pack
+                  stable/power-pack
+                  stable/git-pack
+                  stable/org-pack
+                  stable/clojure-pack
+                  stable/bindings-pack))
 (live-add-packs '(~/.live-packs/selassid-pack))
