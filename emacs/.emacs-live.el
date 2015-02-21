521eecc83c728d9061c01d667ef5532e0cd3d82c
David Selassie <selassid@gmail.com>
2015-02-21 11:50:36 -0800

    Removes org-mode from emacs startup.

--- emacs/.emacs-live.el
+++ emacs/.emacs-live.el
@@ -3,7 +3,7 @@
                   stable/lang-pack
                   stable/power-pack
                   stable/git-pack
-                  stable/org-pack
+                  ;; stable/org-pack
                   stable/clojure-pack
                   stable/bindings-pack))
 (live-add-packs '(~/.live-packs/selassid-pack))
