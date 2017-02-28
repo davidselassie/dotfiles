f48902dd01de321370b1ce2405b855378b0c8a25
David Selassie <selassid@gmail.com>
2017-02-27 17:29:57 -0800

    Big emacs init upgrade.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,10 +1,10 @@
-(setq custom-file "~/.emacs.d/customize.el")
-(load custom-file)
-
-(server-start)
-
-(defvar autosave-dir "~/.emacs.d/auto-save-files/")
-(defvar backup-dir "~/.emacs.d/backup-files/")
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file 'noerror)
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+(setq vc-follow-symlinks t)
 
 (require 'package)
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
@@ -16,55 +16,62 @@
   (require 'use-package))
 (setq use-package-always-ensure t)
 
-(use-package solarized-theme)
+(setq-default word-wrap t)
+(setq-default tab-width 4)
+(setq-default indent-tabs-mode nil)
+
+(setq mouse-wheel-progressive-speed 1)
+(setq mouse-wheel-scroll-amount '(1))
+
+(use-package exec-path-from-shell
+  :config
+  (when (memq window-system '(mac ns))
+    (exec-path-from-shell-initialize)))
+
+(global-hl-line-mode 1)
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(use-package rainbow-delimiters
+  :config
+  (add-hook 'prog-mode-hook
+            #'rainbow-delimiters-mode))
+(show-paren-mode 1)
+(setq-default show-paren-delay 0)
+(use-package color-theme-sanityinc-solarized
+  :config
+  (load-theme 'solarized-light t)
+  (set-face-attribute 'default nil :height 100))
+
 (use-package ensime)
 (use-package company)
 (use-package paredit
   :config
   (add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode))
 (use-package magit)
+
 (use-package git-gutter
   :config
-  (global-git-gutter-mode t))
+  (global-git-gutter-mode t)
+  (git-gutter:linum-setup))
+(global-linum-mode 1)
+(setq line-number-mode nil)
+(setq column-number-mode t)
 
-(add-hook
- 'term-mode-hook
+(use-package markdown-mode)
+
+(add-hook 'term-mode-hook
  (lambda ()
    (setq yas-dont-activate t)))
+(add-hook 'before-save-hook
+ 'delete-trailing-whitespace)
 
 (defun message-buffer-file-name ()
   "Display the full path to the current buffer's file."
   (interactive)
   (message (buffer-file-name (window-buffer (minibuffer-selected-window)))))
+(global-set-key (kbd "C-x C-p") 'message-buffer-file-name)
 
 (setq-default ispell-program-name "hunspell")
 ;(setq-default ispell-really-hunspell t)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(before-save-hook (quote (delete-trailing-whitespace)))
- '(custom-enabled-themes (quote (solarized-light)))
- '(custom-safe-themes
-   (quote
-	("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
- '(dired-use-ls-dired (quote unspecified))
- '(global-hl-line-mode t)
- '(menu-bar-mode nil)
- '(mouse-wheel-progressive-speed nil)
- '(mouse-wheel-scroll-amount (quote (1)))
- '(package-selected-packages
-   (quote
-	(git-gutter solarized-theme paredit magit undo-tree ensime scala-mode use-package)))
- '(tab-width 4)
- '(tool-bar-mode nil)
- '(vc-follow-symlinks t)
- '(word-wrap t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(server-start)
