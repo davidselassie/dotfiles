b28ace355276a92484370d80337a7078891f480c
David Selassie <selassid@gmail.com>
2017-10-12 17:51:12 -0700

    Removes redundant old bin.

--- bin/backup_usbdrive.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DEST="/Volumes/Backup/Backup"
-LIST="$HOME/Development $HOME/Documents $HOME/Desktop $HOME/Dropbox  $HOME/.dotfiles $HOME/Music $HOME/Pictures"
-
-rsync -av --delete $LIST $DEST
