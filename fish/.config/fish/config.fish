af5fd6c2e93e5d625dfe05007a0ce5866b965db5
David Selassie <selassid@gmail.com>
2018-03-05 15:14:56 -0800

    Removes /usr/local/sbin from path, brew does not use it anymore.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,5 @@
 set -x HOMEROOT $HOME/.local
-set -x PATH $HOMEROOT/bin /usr/local/bin /usr/local/sbin $PATH /usr/local/opt/coreutils/libexec/gnubin
+set -x PATH $HOMEROOT/bin /usr/local/bin $PATH /usr/local/opt/coreutils/libexec/gnubin
 set -x MANPATH $HOMEROOT/man $MANPATH /usr/local/opt/coreutils/libexec/gnuman
 set -x PKG_CONFIG_PATH $HOMEROOT/lib/pkgconfig $PKG_CONFIG_PATH
 
