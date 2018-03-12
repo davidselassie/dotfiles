32f27c3d52a85718c45d62abd704de99e8b15528
David Selassie <selassid@gmail.com>
2018-03-12 11:59:18 -0700

    Gets hub completions working.

--- fish/.config/fish/functions/git.fish
+++ fish/.config/fish/functions/git.fish
@@ -1,4 +1,5 @@
-# Defined in - @ line 0
+# Defined in /var/folders/b3/bc9plyg11839hf5p3fwyv7qw0000gn/T//fish.9r6q7E/git.fish @ line 2
 function git --description 'alias git=hub'
-	hub  $argv;
+	complete -c git -w hub
+    hub  $argv;
 end
