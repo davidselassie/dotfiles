b397ffa4e5640202f967a9208b6dc281e4a374be
David Selassie <selassid@gmail.com>
2018-08-07 14:56:48 -0700

    Sets up PATH and MANPATH from almost what /usr/libexec/path_helper would do

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,7 @@
 set -x HOMEROOT $HOME/.local
-set -x PATH $HOMEROOT/bin /usr/local/bin $PATH /usr/local/opt/coreutils/libexec/gnubin
-set -x MANPATH $HOMEROOT/man $MANPATH /usr/local/opt/coreutils/libexec/gnuman
-set -x PKG_CONFIG_PATH $HOMEROOT/lib/pkgconfig $PKG_CONFIG_PATH
+set -x PATH $HOMEROOT/bin /usr/local/bin (cat /etc/paths) /usr/local/opt/coreutils/libexec/gnubin
+set -x MANPATH $HOMEROOT/man (cat /etc/manpaths) /usr/local/opt/coreutils/libexec/gnuman
+set -x PKG_CONFIG_PATH $HOMEROOT/lib/pkgconfig
 
 set -x EDITOR "atom -nw"
 set -x VISUAL $EDITOR
