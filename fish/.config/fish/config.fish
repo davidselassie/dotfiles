5a2d73e886070b07b6c488060ba2f809ab0e8813
David Selassie <dsel@fastmail.com>
2020-02-15 16:50:48 -0800

    use bbedit in EDITOR

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,7 +3,7 @@ set -x PATH $HOMEROOT/bin /usr/local/bin (cat /etc/paths) /usr/local/opt/coreuti
 set -x MANPATH $HOMEROOT/man (cat /etc/manpaths) /usr/local/opt/coreutils/libexec/gnuman
 set -x PKG_CONFIG_PATH $HOMEROOT/lib/pkgconfig
 
-set -x EDITOR "atom -nw"
+set -x EDITOR "bbedit -w"
 set -x VISUAL $EDITOR
 
 set -x MLR_CSV_DEFAULT_RS lf
